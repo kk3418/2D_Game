211b96499e217e21550f2992f2dc5ef5458405bb
kevin.c <kevin.ch.tcg@gmail.com>
2023-02-25 20:55:26 +0800

    [config] add react

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 const path = require("path");
+import react from "@vitejs/plugin-react";
 
 export default {
+  plugins: [react()],
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "src"),
